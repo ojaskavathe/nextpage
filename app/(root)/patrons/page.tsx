44f3e09ec4060e58c746f8592bfaed254c6c50b1
Ojas Kavathe <ojaskavathe@gmail.com>
2024-01-23 15:54:34 +0530

    Fixed permanent redirects

--- app/(root)/patrons/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function PatronCreateForm() {
-  redirect('/patrons/search')
-}
-
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/patrons/:patronId/misc',
+        destination: '/patrons/:patronId/misc/dd',
+        permanent: true
+      },
+      {
+        source: '/patrons',
+        destination: '/patrons/search',
+        permanent: true
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
